11dbcb2d25c5a0d050b719e0162723a7ed6916fb
www <w@w.c>
2019-12-26 15:46:11 +0545

    ,

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 let exp = require("express");
+let request = require("request");
 let server = exp ();
 let {log} = console;
-
+let port = process.env.PORT || 3000;
 server.use (exp.json ());
 
 server.post("/", function(req,res){
-    let body = request.body;
-    log (body)
+	let body = "";
+	req.on("data", e=>body+=e.toString());
+	req.on("end", function(){
+		try {
+			jsn = JSON.parse(body);
+			request(jsn, function(er,stat,body){
+				res.writeHead(200);
+				log(JSON.parse(stat));
+				res.end();
+			});
+		}catch(err){
+			res.writeHead(401);
+			res.end("Error");
+		}
+		res.end();
+	});
 });
-server.listen();
+server.listen(port, e=>log("Server at "+port));
